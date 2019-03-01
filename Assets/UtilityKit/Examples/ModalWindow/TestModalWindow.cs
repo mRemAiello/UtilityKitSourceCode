c8329d376556981e5b9de97d61a76bcfe5d6180a
Mirko <arthas1950@tiscali.it>
2019-03-01 16:40:05 +0100

    Update audio

---
+++ Assets/UtilityKit/Examples/ModalWindow/TestModalWindow.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UtilityKit;
+
+public class TestModalWindow : MonoBehaviour
+{
+    void Start()
+    {
+        ModalPanelDetails modalPanelDetails = new ModalPanelDetails
+        {
+            questionString = "This is test",
+            button1Details = new EventButtonDetails()
+        };
+
+        modalPanelDetails.button1Details.buttonString = "Yes!";
+        modalPanelDetails.button1Details.action = YesFunction;
+
+        modalPanelDetails.button2Details.buttonString = "No!";
+        modalPanelDetails.button2Details.action = NoFunction;
+
+        modalPanelDetails.button3Details.buttonString = "Cancel!";
+        modalPanelDetails.button3Details.action = CancelFunction;
+
+        ModalWindowManager.Choice(modalPanelDetails);
+    }
+
+    void YesFunction()
+    {
+        Debug.Log("You have selected Yes");
+    }
+
+    void NoFunction()
+    {
+        Debug.Log("You have selected No");
+    }
+
+    void CancelFunction()
+    {
+        Debug.Log("You have selected Cancel");
+    }
+}
