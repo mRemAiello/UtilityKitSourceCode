5c44bf94f474b3beeebd715e1e7269444f5b30a6
Mirko <arthas1950@tiscali.it>
2019-03-05 09:50:12 +0100

    Pool Updates

---
+++ Assets/UtilityKit/Examples/Pooling/PoolTest.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UtilityKit;
+
+public class PoolTest : MonoBehaviour
+{
+    public GameObject prefab;
+    public Vector3 spawnPosition;
+
+    void Start()
+    {
+        GameObject spawnItem = Poolable.TryGetPoolable(prefab);
+        spawnItem.transform.position = spawnPosition;
+    }
+}
